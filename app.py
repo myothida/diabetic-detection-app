752b562ab89962fd65b94580fea3502072d1b524
Dr Myo Thida <88795729+myothida@users.noreply.github.com>
2022-06-10 12:42:03 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -26,6 +26,9 @@ from dash.exceptions import PreventUpdate
 
 image_filename =  'VZLogo1.jpg'
 encoded_image = base64.b64encode(open(image_filename, 'rb').read())
+options_yes_no = {"Yes":1, "No":0}
+active = {"Yes":0, "No":1}
+gender = {'F':0, 'M':1}
 
 
 # In[3]:
@@ -59,9 +62,10 @@ app.layout = dbc.Container(
         html.Div([
             dbc.Label("Problem Statement", style={'font-weight': 'bold', "font-family": 'Arial', 'color': '#53a66f',
                                   "font-size": 40,"text-align": "center"}),
-            html.Li("The objective of this project is to construct different machine learning models for predicting diabetes."),            
-            html.Li("The dataset contains information of 60,000 individuals data with 17 features such as \
-                    Age, Glucose Level, Blood pressure, Insulin, BMI, etc.")
+            html.Li("This app predicts the probablity of the patient having diabetes not only based on the demographic features\
+                    but also based on the behaviour of the patient (Smoking, drinking and exercise)."),            
+            html.Li("The prediction model is developed using Logistic Regression and testing on the new data-set gives \
+                    86% accracy")
             
         ]),       
         
@@ -85,12 +89,44 @@ app.layout = dbc.Container(
         html.Br(),
         html.Div([
             html.Div(dbc.Button("Blood Pressure (Top)", color="success", size = 'lg'),style ={'width':'25%'}),
-            html.Div(dbc.Input(id="id-fea4", placeholder="mmHg", type="text"), style ={'width':'25%'}),
+            html.Div(dbc.Input(id="id-fea7", placeholder="mmHg", type="text"), style ={'width':'25%'}),
             html.Div(dbc.Button("Blood Pressure (Bottom)", color="success", size = 'lg'),
                      style ={"margin-left": "55px",'width':'25%'}),
-            html.Div(dbc.Input(id="id-fea5", placeholder="mmHg", type="text"),  style ={'width':'25%'}),            
+            html.Div(dbc.Input(id="id-fea8", placeholder="mmHg", type="text"),  style ={'width':'25%'}),            
         ], style = {'display': 'flex'},),       
         
+        html.Br(),        
+        html.Div([
+            html.Div([
+                dbc.Label("Gender", style={'font-weight': 'bold', "font-family": 'Arial', 'color': '#80007c'}),
+                dbc.RadioItems(
+                options=[{'label':key,'value':gender[key]} for key in gender],
+                    value=gender["F"], id="id-fea9"),
+            ], style = {'width':'25%'}),
+            
+            
+            html.Div([
+                dbc.Label("Do you smoke?", style={'font-weight': 'bold', "font-family": 'Arial', 'color': '#80007c'}),
+                dbc.RadioItems(
+                options=[{'label':key,'value':options_yes_no[key]} for key in options_yes_no],
+                    value=options_yes_no["Yes"], id="id-fea4"),
+            ], style = {'width':'25%'}),
+            
+            html.Div([
+                dbc.Label("Do you Drink Alcohol?", style={'font-weight': 'bold', "font-family": 'Arial', 'color': '#80007c'}),
+                dbc.RadioItems(
+                options=[{'label':key,'value':options_yes_no[key]} for key in options_yes_no],
+                    value=options_yes_no["Yes"], id="id-fea5"),
+            ], style = {'width':'25%'}),
+            
+            html.Div([
+                dbc.Label("Do you Exercise Regulary?", style={'font-weight': 'bold', "font-family": 'Arial', 'color': '#80007c'}),
+                dbc.RadioItems(
+                options=[{'label':key,'value':active[key]} for key in active],
+                    value=active["Yes"], id="id-fea6"),
+            ], style = {'width':'25%'}),
+            
+        ],style ={'display':'flex'}),
         
         html.Br(),
         html.Div([
@@ -112,28 +148,30 @@ app.layout = dbc.Container(
      Input('id-fea3', 'value'),
      Input('id-fea4', 'value'),
      Input('id-fea5', 'value'),
+     Input('id-fea6', 'value'),
+     Input('id-fea7', 'value'),
+     Input('id-fea8', 'value'),
+     Input('id-fea9', 'value'),
      Input('id-predict', 'n_clicks'),
     ]
 )
 def load_data(*args):    
         
-    if(len(args)<6):
+    if(len(args)<10):
         raise PreventUpdate
     else:
         loaded_model = joblib.load('LR_best_classification_model.pkl') 
         result = 'Model loaded .... ' 
         fea_list = []
-        for arg in args[0:5]:
+        for arg in args[0:len(args)-1]:
             if arg is None:
                 result = 'Enter the value .. '
             else:
                 fea_list.append(int(arg))
         
-        if(args[-1]): 
-            if(loaded_model.predict([fea_list])==1):
-                result = "You have high chance of getting Diabetic"
-            else:
-                result = "You have low chance of getting Diabetic"
+        if(args[-1]):             
+            prob_predict = 100*loaded_model.predict_proba([fea_list])
+            result = "You have " + "{:.2f}".format(prob_predict[0,0]) + "% of getting Diabetic"
         
         return result    
 
